9e295cb64071eac6800df458f0a867f771385560
nthoms2 <54376526+nthoms2@users.noreply.github.com>
2019-08-26 13:40:17 -0500

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+/*
+Nolan Thoms
+C++ Fall Semester 2019
+8 / 29 / 2019
+Lab 1
+making hello world and uploading it
+*/
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World, Orange";
+
+		return 0;
+}
